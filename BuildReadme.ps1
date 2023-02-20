ef12b0055d6a7032ade1266e73b73410a1d56b0f
innovatodev <innovatodev@gmail.com>
2023-02-20 18:57:09 +0100

    0.0.2

--- BuildReadme.ps1
+++ BuildReadme.ps1
@@ -7,6 +7,23 @@ $ErrorActionPreference = 'Stop'
 $CountryGlobal = Import-Csv -Path '.\csv\CountryGlobal\CountryGlobal.csv'
 $CountryGlobal_IPV4 = Import-Csv -Path '.\csv\CountryGlobal\CountryGlobal_IPV4.csv'
 $CountryGlobal_IPV6 = Import-Csv -Path '.\csv\CountryGlobal\CountryGlobal_IPV6.csv'
+$RegionGlobal_Reserved = Import-Csv -Path '.\csv\RegionGlobal\RegionGlobal_Reserved.csv'
+$RegionGlobal_Reserved_IPV4 = Import-Csv -Path '.\csv\RegionGlobal\RegionGlobal_Reserved_IPV4.csv'
+$RegionGlobal_Reserved_IPV6 = Import-Csv -Path '.\csv\RegionGlobal\RegionGlobal_Reserved_IPV6.csv'
+$RegionGlobal_Available = Import-Csv -Path '.\csv\RegionGlobal\RegionGlobal_Available.csv'
+$RegionGlobal_Available_IPV4 = Import-Csv -Path '.\csv\RegionGlobal\RegionGlobal_Available_IPV4.csv'
+$RegionGlobal_Available_IPV6 = Import-Csv -Path '.\csv\RegionGlobal\RegionGlobal_Available_IPV6.csv'
+
+$IANA_Allocated = Import-Csv -Path '.\csv\IANA\IANA_Allocated.csv'
+$IANA_Allocated_IPV4 = Import-Csv -Path '.\csv\IANA\IANA_Allocated_IPV4.csv'
+$IANA_Allocated_IPV6 = Import-Csv -Path '.\csv\IANA\IANA_Allocated_IPV6.csv'
+$IANA_Reserved = Import-Csv -Path '.\csv\IANA\IANA_Reserved.csv'
+$IANA_Reserved_IPV4 = Import-Csv -Path '.\csv\IANA\IANA_Reserved_IPV4.csv'
+$IANA_Reserved_IPV6 = Import-Csv -Path '.\csv\IANA\IANA_Reserved_IPV6.csv'
+$IANA_Available = Import-Csv -Path '.\csv\IANA\IANA_Available.csv'
+$IANA_Available_IPV4 = Import-Csv -Path '.\csv\IANA\IANA_Available_IPV4.csv'
+$IANA_Available_IPV6 = Import-Csv -Path '.\csv\IANA\IANA_Available_IPV6.csv'
+
 $Regions = $CountryGlobal | Select-Object region -Unique
 $Countries = $CountryGlobal | Select-Object country -Unique
 #endregion data
@@ -15,17 +32,102 @@ $README = "# WorldIP
 
 {Country_Count} Countries in {Region_Count} regions
 
-{CIDR_Count} CIDR - {CIDR_IPV4_Count} IPV4 - {CIDR_IPV6_Count} IPV6
+Regions Allocated/Assigned CIDR :
+
+{Global_Regions_AA_STATS}
+{Regions_AA_STATS}
+
+Regions Reserved CIDR :
+
+{Global_Regions_Reserved_STATS}
+{Regions_Reserved_STATS}
+
+Regions Available CIDR :
+
+{Global_Regions_Available_STATS}
+{Regions_Available_STATS}
+
+IANA Allocated/Assigned CIDR :
+
+{Global_IANA_AA_STATS}
+{IANA_AA_STATS}
+
+IANA Reserved CIDR :
+
+{IANA_Reserved_STATS}
+
+IANA Available CIDR :
+
+{IANA_Available_STATS}
 "
+
+#region Header
 
 $README = $README -replace '{Country_Count}', $($Countries.Count)
 $README = $README -replace '{Region_Count}', $($Regions.Count)
-$README = $README -replace '{CIDR_Count}', $($CountryGlobal.Count)
-$README = $README -replace '{CIDR_IPV4_Count}', $($CountryGlobal_IPV4.Count)
-$README = $README -replace '{CIDR_IPV6_Count}', $($CountryGlobal_IPV6.Count)
 
+#endregion Header
+
+#region Regions
+
+# Allocated/Assigned
+$README = $README -replace '{Global_Regions_AA_STATS}', "- global : $($CountryGlobal.Count) Total | $($CountryGlobal_IPV4.Count) IPV4 | $($CountryGlobal_IPV6.Count) IPV6"
+$buffer = ""
+foreach ($region in $Regions) {
+    $count = ($CountryGlobal | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv4 = ($CountryGlobal_IPV4 | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv6 = ($CountryGlobal_IPV6 | Where-Object { $_.region -eq $($region.region) }).Count
+    $buffer += "- $($region.region) : $($count) Total | $($count_ipv4) IPV4 | $($count_ipv6) IPV6`n"
+}
+$README = $README -replace '{Regions_AA_STATS}', ($buffer).Trim()
+
+# Reserved
+$README = $README -replace '{Global_Regions_Reserved_STATS}', "- global : $($RegionGlobal_Reserved.Count) Total | $($RegionGlobal_Reserved_IPV4.Count) IPV4 | $($RegionGlobal_Reserved_IPV6.Count) IPV6"
+$buffer = ""
+foreach ($region in $Regions) {
+    $count = ($RegionGlobal_Reserved | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv4 = ($RegionGlobal_Reserved_IPV4 | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv6 = ($RegionGlobal_Reserved_IPV6 | Where-Object { $_.region -eq $($region.region) }).Count
+    $buffer += "- $($region.region) : $($count) Total | $($count_ipv4) IPV4 | $($count_ipv6) IPV6`n"
+}
+$README = $README -replace '{Regions_Reserved_STATS}', ($buffer).Trim()
+
+# Available
+$README = $README -replace '{Global_Regions_Available_STATS}', "- global : $($RegionGlobal_Available.Count) Total | $($RegionGlobal_Available_IPV4.Count) IPV4 | $($RegionGlobal_Available_IPV6.Count) IPV6"
+$buffer = ""
+foreach ($region in $Regions) {
+    $count = ($RegionGlobal_Available | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv4 = ($RegionGlobal_Available_IPV4 | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv6 = ($RegionGlobal_Available_IPV6 | Where-Object { $_.region -eq $($region.region) }).Count
+    $buffer += "- $($region.region) : $($count) Total | $($count_ipv4) IPV4 | $($count_ipv6) IPV6`n"
+}
+$README = $README -replace '{Regions_Available_STATS}', ($buffer).Trim()
+
+#endregion Regions
+
+#region IANA
+
+# Allocated/Assigned
+$README = $README -replace '{Global_IANA_AA_STATS}', "- global : $($IANA_Allocated.Count) Total | $($IANA_Allocated_IPV4.Count) IPV4 | $($IANA_Allocated_IPV6.Count) IPV6"
+$buffer = ""
+foreach ($region in $Regions) {
+    $count = ($IANA_Allocated | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv4 = ($IANA_Allocated_IPV4 | Where-Object { $_.region -eq $($region.region) }).Count
+    $count_ipv6 = ($IANA_Allocated_IPV6 | Where-Object { $_.region -eq $($region.region) }).Count
+    $buffer += "- $($region.region) : $($count) Total | $($count_ipv4) IPV4 | $($count_ipv6) IPV6`n"
+}
+$README = $README -replace '{IANA_AA_STATS}', ($buffer).Trim()
+# Reserved
+$README = $README -replace '{IANA_Reserved_STATS}', "- global : $($IANA_Reserved.Count) Total | $($IANA_Reserved_IPV4.Count) IPV4 | $($IANA_Reserved_IPV6.Count) IPV6"
+# Available
+$README = $README -replace '{IANA_Available_STATS}', "- global : $($IANA_Available.Count) Total | $($IANA_Available_IPV4.Count) IPV4 | $($IANA_Available_IPV6.Count) IPV6"
+
+#endregion IANA
+
+# Print
 "<----------------------->"
 ($README).Trim()
 "<----------------------->"
 
+# Write
 ($README).Trim() | Set-Content .\README.md
--- README.md
+++ README.md
@@ -2,4 +2,46 @@
 
 238 Countries in 5 regions
 
-298956 CIDR - 237542 IPV4 - 61414 IPV6
+Regions Allocated/Assigned CIDR :
+
+- global : 298956 Total | 237542 IPV4 | 61414 IPV6
+- ripencc : 116185 Total | 90837 IPV4 | 25348 IPV6
+- apnic : 65183 Total | 51443 IPV4 | 13740 IPV6
+- arin : 80391 Total | 71594 IPV4 | 8797 IPV6
+- afrinic : 5859 Total | 4601 IPV4 | 1258 IPV6
+- lacnic : 31338 Total | 19067 IPV4 | 12271 IPV6
+
+Regions Reserved CIDR :
+
+- global : 133898 Total | 3072 IPV4 | 130826 IPV6
+- ripencc : 80355 Total | 329 IPV4 | 80026 IPV6
+- apnic : 1395 Total | 487 IPV4 | 908 IPV6
+- arin : 38079 Total | 1711 IPV4 | 36368 IPV6
+- afrinic : 0 Total | 0 IPV4 | 0 IPV6
+- lacnic : 14069 Total | 545 IPV4 | 13524 IPV6
+
+Regions Available CIDR :
+
+- global : 137279 Total | 950 IPV4 | 136329 IPV6
+- ripencc : 650 Total | 4 IPV4 | 646 IPV6
+- apnic : 83527 Total | 942 IPV4 | 82585 IPV6
+- arin : 26600 Total | 4 IPV4 | 26596 IPV6
+- afrinic : 0 Total | 0 IPV4 | 0 IPV6
+- lacnic : 26502 Total | 0 IPV4 | 26502 IPV6
+
+IANA Allocated/Assigned CIDR :
+
+- global : 1373 Total | 1327 IPV4 | 46 IPV6
+- ripencc : 101 Total | 82 IPV4 | 19 IPV6
+- apnic : 196 Total | 180 IPV4 | 16 IPV6
+- arin : 949 Total | 942 IPV4 | 7 IPV6
+- afrinic : 62 Total | 60 IPV4 | 2 IPV6
+- lacnic : 65 Total | 63 IPV4 | 2 IPV6
+
+IANA Reserved CIDR :
+
+- global : 3494 Total | 496 IPV4 | 2998 IPV6
+
+IANA Available CIDR :
+
+- global : 2396 Total | 26 IPV4 | 2370 IPV6
